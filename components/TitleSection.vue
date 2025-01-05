66101b617e43c53332639bbe2970e6ef5490bb34
l31dev <clauinangelo@proton.me>
2025-01-05 17:20:53 +0100

    mobile

--- components/TitleSection.vue
+++ components/TitleSection.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="relative">
-    <a href="" class="uppercase absolute -top-1 md:text-xl text-base md:-right-7 -right-4 purple font-black glow-text">Approved by momo</a>
+    <a href="https://tracker.gg/valorant/profile/riot/Moiragh%23EUWD/overview" class="uppercase absolute -top-1 md:text-xl text-base md:-right-7 -right-4 purple font-black glow-text">Approved by momo</a>
     <h1 class="font-black italic md:text-7xl text-5xl">Delikesance</h1>
     <h2 class="font-black md:-mt-5 -mt-4 -ml-2 md:text-3xl text-2xl purple glow-text">潮解</h2>
   </div>
