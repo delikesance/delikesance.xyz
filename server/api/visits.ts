b375c6cb2b5e7eca29fdaa407b6181e1e536f676
l31dev <clauinangelo@proton.me>
2025-01-05 15:48:23 +0100

    mobile

--- server/api/visits.ts
+++ server/api/visits.ts
@@ -1,9 +1,14 @@
+import { Redis } from "@upstash/redis";
+
 export default defineEventHandler(async event => {
-  const storage = useStorage('data')
-  
-  let visits = await storage.getItem<number>('pageVisits') || 0
+  const redis = new Redis({
+    url: 'https://suited-eft-53309.upstash.io',
+    token: 'AdA9AAIjcDFhOWM5YjYwNzZlZjY0N2Y2YWRjN2ZhODVhMzE3Zjg4Y3AxMA',
+  })
+
+  let visits = await redis.get<number>('visits') || 0
   visits++
 
-  await storage.setItem("visits", visits)
-  return {visits}
+  await redis.set("visits", visits)
+  return { visits }
 })
