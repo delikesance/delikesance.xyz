34000a1b7b441e23e8e63a23cc95cd4e7b057b4b
l31dev <clauinangelo@proton.me>
2025-01-05 18:20:15 +0100

    mobile

--- server/api/visits.ts
+++ server/api/visits.ts
@@ -1,38 +1,21 @@
-import { createHash } from 'crypto';
-import { Redis } from '@upstash/redis';
+import { Redis } from "@upstash/redis";
 
-interface RequestEvent {
-  node: {
-    req: {
-      headers: {
-        'x-forwarded-for'?: string;
-      };
-      socket: {
-        remoteAddress?: string;
-      };
-    };
-  };
-}
-
-export default async function handler(event: RequestEvent) {
+export default defineEventHandler(async (event) => {
   const redis = new Redis({
     url: 'https://suited-eft-53309.upstash.io',
     token: 'AdA9AAIjcDFhOWM5YjYwNzZlZjY0N2Y2YWRjN2ZhODVhMzE3Zjg4Y3AxMA',
   });
 
-  const ip = event.node.req.headers['x-forwarded-for'] || event.node.req.socket.remoteAddress || '';
-  const hashedIp = createHash('sha256').update(ip).digest('hex');
-
+  const ip = event.node.req.headers['x-forwarded-for'] || event.node.req.socket.remoteAddress;
   const totalVisits = await redis.incr('totalVisits');
 
-  const hasVisited = await redis.sismember('uniqueVisitors', hashedIp);
+  const hasVisited = await redis.sismember('uniqueVisitors', ip);
 
   if (!hasVisited) {
-    await redis.sadd('uniqueVisitors', hashedIp);
+    await redis.sadd('uniqueVisitors', ip);
   }
 
   const uniqueVisits = await redis.scard('uniqueVisitors');
-
   return { totalVisits, uniqueVisits };
-}
+});
 
